23fac25d49a3db92999d58353361cd48df2c10ad
lunixchewbahka <lunixchewbahka@gmail.com>
2017-11-30 11:26:51 +0800

    Sumitted my solution for Rust exercism problem #2 - gigasecond

--- exercism/rust/gigasecond/src/lib.rs
+++ exercism/rust/gigasecond/src/lib.rs
@@ -2,6 +2,11 @@ extern crate chrono;
 use chrono::*;
 
 // Returns a Utc DateTime one billion seconds after start.
-pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!()
+pub fn after(date: DateTime<Utc>) -> DateTime<Utc> {
+    // for more information about exponentiation in rust https://doc.rust-lang.org/nightly/std/?search=pow
+    let x: i64 = 10;
+    let temp_result = x.pow64(9));
+    assert_eq!(temp_result, 1_000_000_000); // no errors? I guess it's a match
+    assert_eq!(temp_result, 1_000_000_000, "Testing if temp_result == 1_000_000_000", a, b); // same, no errors.
+    date + Duration::seconds(x.pow64(9)) // calculate and return result;
 }
