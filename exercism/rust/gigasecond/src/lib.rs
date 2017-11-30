ff08608a2b2d9e451c0997e8e10f85940a5b0d96
lunixchewbahka <lunixchewbahka@gmail.com>
2017-11-30 11:36:30 +0800

    Added comments for clarification and removed some tests

--- exercism/rust/gigasecond/src/lib.rs
+++ exercism/rust/gigasecond/src/lib.rs
@@ -4,9 +4,9 @@ use chrono::*;
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(date: DateTime<Utc>) -> DateTime<Utc> {
     // for more information about exponentiation in rust https://doc.rust-lang.org/nightly/std/?search=pow
-    let x: i64 = 10;
-    let temp_result = x.pow64(9));
-    assert_eq!(temp_result, 1_000_000_000); // no errors? I guess it's a match
-    assert_eq!(temp_result, 1_000_000_000, "Testing if temp_result == 1_000_000_000", a, b); // same, no errors.
-    date + Duration::seconds(x.pow64(9)) // calculate and return result;
+    let x: i64 = 10; // x takes a value of 10 which is of type i64 (64-bit integer)
+    let giga_sec_val = x.pow(9); // 10 raise to the power of 9
+    assert_eq!(giga_sec_val, 1_000_000_000); // no errors? I guess it's a match
+    assert_eq!(giga_sec_val, 1_000_000_000, "Testing if temp_result == 1_000_000_000"); // same, no errors.
+    date + Duration::seconds(giga_sec_val) // calculate date + duration which is converted to seconds and return result;
 }
