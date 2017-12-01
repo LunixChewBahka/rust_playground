117e8c5eb59d610f27beada32af971c4bfbe3ecc
lunixchewbahka <lunixchewbahka@gmail.com>
2017-12-02 00:20:42 +0800

    Created adder project to get started with tests in rust programming

---
+++ learning_rust/adder/target/debug/adder-1818e469b3318d39.d
@@ -0,0 +1,1 @@
+/home/lunixchewbahka/rust_proj_dir/learning_rust/adder/target/debug/adder-1818e469b3318d39: /home/lunixchewbahka/rust_proj_dir/learning_rust/adder/src/lib.rs
---
+++ learning_rust/adder/target/debug/libadder.d
@@ -0,0 +1,1 @@
+/home/lunixchewbahka/rust_proj_dir/learning_rust/adder/target/debug/libadder.rlib: /home/lunixchewbahka/rust_proj_dir/learning_rust/adder/src/lib.rs
