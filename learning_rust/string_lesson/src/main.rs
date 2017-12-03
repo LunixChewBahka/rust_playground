f9a80b191f835772b6051c54781d4353cc135230
lunixchewbahka <lunixchewbahka@gmail.com>
2017-12-03 17:51:36 +0800

    Playing around with bitflags and String operations.

--- learning_rust/string_lesson/src/main.rs
+++ learning_rust/string_lesson/src/main.rs
@@ -9,4 +9,21 @@ fn main() {
     */
     let hello_world = "Hello from the other side.";
     println!("{:?}", hello_world);
+
+    let mut owned_string = String::from("hello ".to_owned());
+    let borrowed_string: &str = "world";
+
+    owned_string.push_str(borrowed_string);
+    println!("{}", owned_string);
+
+    let msg = "hello world".to_string();
+    print_me(msg);
+
+    let answer = 42;
+    println!("Hello {}", answer);
+}
+
+
+fn print_me(msg: String) {
+    println!("the message is {}", msg);
 }
